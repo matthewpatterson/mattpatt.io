bfed56171e9b73306da0c5a901e27bca197d0e4d
Matthew Patterson <matthew.s.patterson@gmail.com>
2014-07-19 00:34:21 -0500

    Move homepage and layout to ERB

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'thin', '1.6.2'
 gem 'sinatra', '1.4.5'
+gem 'yui-compressor', '0.12.0'
 
 group :development do
   gem 'shotgun', '0.9'
--- app.rb
+++ app.rb
@@ -1,3 +1,12 @@
-get '/' do
-  'Hello world!'
+get "/home" do
+  erb :homepage
+end
+
+get "/" do
+  call! env.merge("PATH_INFO" => "/home")
+end
+
+not_found do
+  @title = "File Not Found - "
+  erb :error_404
 end
