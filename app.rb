1026b13a7fd4ed2617dee7fca21ae51fa13c4214
Matthew Patterson <matthew.s.patterson@gmail.com>
2014-07-19 22:56:14 -0500

    Move About Me, About this Site to ERB

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ get "/home" do
   erb :homepage
 end
 
+get "/about" do
+  @title = "About Me - "
+  erb :about
+end
+
+get "/this" do
+  @title = "About This Site - "
+  erb :site
+end
+
 get "/" do
   call! env.merge("PATH_INFO" => "/home")
 end
